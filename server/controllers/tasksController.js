de74f1b78d3aae4813e5cc68017d47586d695353
RazaNaqsh <owaisraza1704@gmail.com>
2023-09-06 07:46:00 +0530

    Task controllers for get and post

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -1,7 +1,25 @@
 import mongoose from "mongoose";
 import express from "express";
+import TaskModel from "../models/taskMode";
 
-export const getTasks = async (req, res) => {};
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await TaskModel.find();
+
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
 export const createTask = async (req, res) => {
   const task = req.body;
+
+  const newTask = new TaskModel(task);
+  try {
+    await newTask.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
